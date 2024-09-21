79374e6972406185f2452334c87e6dadac083a7a
Sauli Enckelman <137370213+enckelman@users.noreply.github.com>
2024-09-22 00:21:09 +0800

    Disabled buffers and added setups

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,13 @@
+return {
+  -- { "windwp/nvim-spectre", enabled = false }
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+}
